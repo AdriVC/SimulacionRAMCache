d77241ce8985e578eccc817c6415161fa7af70ad
Adriana Vega <adriana.vega@unitec.edu>
2016-06-06 19:26:35 -0600

    paraMain

---
+++ Tarea1_AVASFRMPOC.cpp
@@ -0,0 +1,10 @@
+//Tarea 1 Organizacion de Computadoras
+//Ing. Pablo Meraz
+//Por: Adriana Vega, Ambar Suarez, Fernando Reyes, Melvin Portillo, Oscar Carranza
+
+char**
+
+int main(int argc, char const *argv[]){
+	
+	return 0;
+}
